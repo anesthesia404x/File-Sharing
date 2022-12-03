60045996c37d79cbc8890c92666f15981563c363
anesthesia404x <115077016+anesthesia404x@users.noreply.github.com>
2022-12-03 18:05:25 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,7 +88,7 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
+                    InlineKeyboardButton("😊 Channel", url = "t.me/xproject13"),
                     InlineKeyboardButton("🔒 Close", callback_data = "close")
                 ]
             ]
